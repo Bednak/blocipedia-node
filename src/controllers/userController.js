5477be18b4555027a66b07b2604940192efb7223
Kamil <kamilbednarz91@gmail.com>
2019-08-22 17:13:21 -0500

    Added Collaborator function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -45,7 +45,7 @@ module.exports = {
         res.redirect("users/signIn");
       } else {
         req.flash("notice", "You've successfully signed in!");
-        res.redirect("/wikis");
+        res.redirect("/");
       }
     })
   },
@@ -120,6 +120,18 @@ module.exports = {
       req.flash("notice", "You now have premium membership!");
       res.redirect("/");
     })
+  },
+
+  showCollaborations(req, res, next){
+    userQueries.getUser(req.user.id, (err, result) => {
+      user = result["user"];
+      collaborations = result["collaborations"];
+      if(err || user == null){
+        res.redirect(404, "/");
+      } else {
+        res.render("users/collaborations", {user, collaborations});
+      }
+    });
   }
 
 }
